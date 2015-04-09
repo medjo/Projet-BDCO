a22394635830e9150ea9828840766a9dfb7e82b3
Mordokkai <s.guy09@laposte.net>
2015-04-09 19:56:04 +0200

    Connexion à la BD

---
+++ BattleShip/src/BattleShip.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+	
+public class BattleShip {
+	
+	public static void main (String[] args) throws SQLException {
+		TheConnection theConnection = new TheConnection(new ConnectionInfo("jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1","guys","guys"));
+		theConnection.open();
+		
+		//Corps
+		
+		
+		theConnection.close();
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
---
+++ BattleShip/src/TheConnection.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class TheConnection {
+	private ConnectionInfo info;
+	private Connection conn;
+	public void open() {
+		try{
+			//Enregistrement du driver oracle
+			System.out.print("Chargement du driver oracle");
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			System.out.println("Driver chargé");
+			
+			//Etablissement de la connexion
+			System.out.print("Connection en cours à la base de données");
+			conn = DriverManager.getConnection(info.CONN_URL, info.USER, info.PASSWD);
+			System.out.println("Connecté");
+		} catch (SQLException e) {
+			System.err.println("Echec");
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	public void close() throws SQLException {
+		System.out.println("Deconnecté");
+		conn.close(); }
+	Connection getConnection() {return conn;}
+	public TheConnection (ConnectionInfo info) {this.info=info;}
+}
