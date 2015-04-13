88e069b52bc1e39b310d6e5f57920fd8d871376a
Mordokkai <s.guy09@laposte.net>
2015-04-13 19:56:18 +0200

    reorganisation classes + enum DIRECTION

--- BattleShip/src/modele/Deplacement.java
+++ BattleShip/src/modele/Deplacement.java
@@ -1,5 +1,17 @@
 package modele;
 
 public class Deplacement extends Action{
-	 
+private Direction dir;
+	
+	public Deplacement(Direction dir) {
+		this.dir=dir;
+	}
+	
+	public void setDir(Direction dir) {
+		this.dir=dir;
+	}
+	
+	public Direction getDir() {
+		return this.dir;
+	}
 }
---
+++ BattleShip/src/modele/Direction.java
@@ -0,0 +1,5 @@
+package modele;
+
+public enum Direction {
+	BABORD, TRIBORD, DEVANT, DERRIERE
+}
