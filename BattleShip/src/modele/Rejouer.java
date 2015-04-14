f30355044400406defb5845f9e31b6b5c481df01
Mordokkai <s.guy09@laposte.net>
2015-04-14 18:23:06 +0200

    commentaire

--- BattleShip/src/modele/Rejouer.java
+++ BattleShip/src/modele/Rejouer.java
@@ -12,5 +12,9 @@ public class Rejouer{
 	public void voirPartie(String idPartie) {
 		Historique h = new Historique(idPartie); //On instancie l'historique sélectionné
 	}
-	
+	/*Lien avec l'interface graphique: 
+	 * -> lorsque l'on entre dans la fenêtre de l'historique on a la liste des parties qui s'affiche.
+	 * -> si on veut voir une partie on appel la méthode, si on veut rafaichir on appel la meme méthode
+	 * et on se déplacera au dernier élément.
+	 */
 }
