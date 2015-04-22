f4e3d077a763b1a667b8d74eb3eb6eb87d3fe20b
Mordokkai <S.Guy09@laposte.net>
2015-04-22 12:26:49 +0200

    poursuite archi

--- BattleShip/src/modele/Partie.java
+++ BattleShip/src/modele/Partie.java
@@ -1,6 +1,8 @@
 package modele;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import jdbc.TheConnection;
 import jdbc.SimpleQuery;
 
@@ -8,19 +10,34 @@ public class Partie {
 	private int numTour;
 	//private ArrayList<Ship> bateaux;
 	
-	public void recherchePartie(TheConnection theConnection) {
+	public ArrayList<idJoueur> joueursDispos(TheConnection theConnection) {
+		ArrayList<idJoueur> listeJoueurEnAttente = new ArrayList<idJoueur>(); 
 		SimpleQuery req = new SimpleQuery(theConnection.getConnection(),"SELECT * FROM enAttente");
 		req.execute();
 		ResultSet res = req.getResult();
 		try{
 			while(res.next()){
-				listeParties.add(new InfoPartie(theConnection, res.getString(1), res.getString(2), res.getBoolean(3)));
+				//on crée une liste de pseudo et nombre de parties des joueurs en attente
+				listeJoueurEnAttente.add(new idJoueur(res.getString(1),res.getDouble(2)));
 			}
 		} catch (Exception e) {
 			
 		}
 		
 		req.close();
+		return listeJoueurEnAttente;
 	}
 	
+	public idJoueur selectionnerAdv(ArrayList<idJoueur> listeJoueursEnAttente){
+		int i=0;
+		idJoueur joueurMin;
+		joueurMin = listeJoueursEnAttente.get(0);
+		while(i<listeJoueursEnAttente.size()){
+			if(listeJoueursEnAttente.get(i).getNbParties()<joueurMin.getNbParties()){
+				joueurMin=listeJoueursEnAttente.get(i);
+			}
+			i++;
+		}
+		return joueurMin;
+	}
 }
