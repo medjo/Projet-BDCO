96948b7396952cbe80fafdd2989a6967d7adf9c0
loic13 <turin.loic@gmail.com>
2015-04-22 16:53:40 +0200

    Action et Tir implementent save, execute
    save et execute sont abstract dans la super Action.

--- BattleShip/src/modele/Action.java
+++ BattleShip/src/modele/Action.java
@@ -1,5 +1,45 @@
 package modele;
 
-public abstract class Action {
+import jdbc.TheConnection;
 
+public abstract class Action {
+	private int idBateau;
+	private int idPartie;
+	private int nTour;
+	private int nAction;
+	
+	public Action(int idBateau, int idPartie, int nTour, int nAction) {
+		this.idBateau=idBateau;
+		this.idPartie=idPartie;
+		this.nAction = nAction;
+		this.nTour = nTour;
+	}
+	
+	/**
+	 * execute l'action au niveau de la BD ie met à jour l'etat des bateaux
+	 * @param theConnection
+	 */
+	public abstract void execute(TheConnection theConnection);
+	
+	/**
+	 * sauvegarde l'action au niveau de la BD dans l'historique de la partie 
+	 * @param theConnection
+	 */
+	public abstract void save(TheConnection theConnection);
+	
+	public int getIdBateau(){
+		return idBateau;
+	}
+	
+	public int getIdPartie(){
+		return idPartie;
+	}
+	
+	public int getNTour(){
+		return nTour;
+	}
+	
+	public int getNAction(){
+		return nAction;
+	}
 }
