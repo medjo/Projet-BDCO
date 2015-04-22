7f96b7cb8f223f71d847f2048ccabd19b98e0fdf
Mordokkai <S.Guy09@laposte.net>
2015-04-22 11:38:16 +0200

    poursuite architecture

--- BattleShip/src/modele/Deplacement.java
+++ BattleShip/src/modele/Deplacement.java
@@ -2,9 +2,11 @@ package modele;
 
 public class Deplacement extends Action{
 private Direction dir;
+private Ship ship;
 	
-	public Deplacement(Direction dir) {
+	public Deplacement(Direction dir, Ship ship) {
 		this.dir=dir;
+		this.ship=ship;
 	}
 	
 	public void setDir(Direction dir) {
@@ -13,5 +15,23 @@ private Direction dir;
 	
 	public Direction getDir() {
 		return this.dir;
+	}
+	
+	public void execute() throws ExceptionDeplacement{
+		if(ship.dir==Direction.NORD && this.dir==Direction.SUD) {
+			throw new ExceptionDeplacement();
+		}
+		else if(ship.dir==Direction.SUD && this.dir==Direction.NORD) {
+			throw new ExceptionDeplacement();
+		}
+		else if(ship.dir==Direction.OUEST && this.dir==Direction.EST) {
+			throw new ExceptionDeplacement();
+		}
+		else if(ship.dir==Direction.EST && this.dir==Direction.OUEST) {
+			throw new ExceptionDeplacement();
+		}
+		else {
+			
+		}
 	}
 }
