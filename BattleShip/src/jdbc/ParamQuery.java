02d778ed007dc2c0165a5940523c3c500dfa6c29
loic13 <turin.loic@gmail.com>
2015-04-23 17:53:48 +0200

    gestion de des exception SQL plus fine
    les exception de constraint violation sont renvoyées plus dans ParamQuery
    et SimpleQuery

--- BattleShip/src/jdbc/ParamQuery.java
+++ BattleShip/src/jdbc/ParamQuery.java
@@ -24,13 +24,29 @@ public class ParamQuery extends Request{
 		this.pstmt=st;
 	}
 	
-	public ResultSet execute() {
+	/**
+	 * execute la requete sql sur la BD.
+	 * Les exceptions sql sont traitées ici, sauf les exceptions de violation de contrainte
+	 * qui sont renvoyées plus haut
+	 * @return resultat de la requete
+	 * @throws SQLException
+	 */
+	public ResultSet execute() throws SQLException {
 		try {
 			ResultSet result =pstmt.executeQuery();
 			return result;
 		}
 		catch (SQLException e) {
+			String message = e.getMessage();
+			int errorCode = e.getErrorCode();
+			String sqlState = e.getSQLState();
+			if(sqlState == "23000"){ // integrity violation constraint
+				throw e;
+			}
 			System.err.println("Echec de l'éxécution de la requête");
+			System.out.println("Message = "+message);
+			System.out.println("SQLState = "+sqlState);
+			System.out.println("ErrorCode = "+errorCode);
 			e.printStackTrace(System.err);
 			return  null; //Il faut retourner un type même si la reque est fausse.
 		}
--- BattleShip/src/jdbc/SimpleQuery.java
+++ BattleShip/src/jdbc/SimpleQuery.java
@@ -14,18 +14,35 @@ public class SimpleQuery extends Request{
 		}
 	}
 	
-	public ResultSet execute() {
+	/**
+	 * execute la requete sql sur la BD.
+	 * Les exceptions sql sont traitées ici, sauf les exceptions de violation de contrainte
+	 * qui sont renvoyées plus haut
+	 * @return resultat de la requete
+	 * @throws SQLException
+	 */
+	public ResultSet execute() throws SQLException {
 		try {
 			
 			result = stmt.executeQuery(query);
 			return result;
 		}
 		catch (SQLException e) {
-			System.err.println("Echec à l'éxécution de la requête");
+			String message = e.getMessage();
+			int errorCode = e.getErrorCode();
+			String sqlState = e.getSQLState();
+			if(sqlState == "23000"){ // integrity violation constraint
+				throw e;
+			}
+			System.err.println("Echec de l'éxécution de la requête");
+			System.out.println("Message = "+message);
+			System.out.println("SQLState = "+sqlState);
+			System.out.println("ErrorCode = "+errorCode);
 			e.printStackTrace(System.err);
-			return  result; //Il faut retourner un type même si la requete est fausse.
+			return  null; //Il faut retourner un type même si la reque est fausse.
 		}
 	}
+	
 	public int updateQuery(){
 		try {
 			
